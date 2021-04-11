23e20ccf4e80c1e5e788bcd14e2c2d4ec10a4d4d
1999Chen <lsy1999lsy@gmail.com>
2021-04-11 21:37:51 +0200

    tree

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+
 public class BinaryTree {
 
 
@@ -55,17 +56,155 @@ public class BinaryTree {
         return false;
     }
 
-    public ArrayList inOrder(){
-        ArrayList<BinaryTreeNode>list=new ArrayList<>();
+
+    public ArrayList<BinaryTreeNode> inOrder(){
+
+        ArrayList<BinaryTreeNode> list=new ArrayList();
+        inOrderTraversal(root,list);
+        if (list.isEmpty()){
+            return null;
+        }
+        return list;
+    }
+
+    public ArrayList<BinaryTreeNode> preOrder(){
+
+        ArrayList<BinaryTreeNode> list=new ArrayList();
+        preOrderTraversal(root,list);
+        if (list.isEmpty()){
+            return null;
+        }
+        return list;
+    }
+
+    public ArrayList<BinaryTreeNode> postOrder(){
+
+        ArrayList<BinaryTreeNode> list=new ArrayList();
+        postOrderTraversal(root,list);
+        if (list.isEmpty()){
+            return null;
+        }
+        return list;
+    }
 
 
+    public ArrayList<BinaryTreeNode> levelOrder(){
 
+        ArrayList<BinaryTreeNode> list=new ArrayList();
 
-
-
-
+        int height=height();
+        for (int i=0;i<height;i++){
+            levelOrderTraversal(root,list,i);
+        }
+        return levelOrder();
 
     }
+
+
+
+    private void levelOrderTraversal(BinaryTreeNode node, ArrayList<BinaryTreeNode> list,int no){
+
+        if (node!=null)
+        {
+            levelOrderTraversal(node.getLeftChild(),list,no);
+            levelOrderTraversal(node.getRightChild(),list,no);
+
+        }
+    }
+
+    public int height(){
+        if (isEmpty())
+        {
+            return -1;
+        }
+        else{
+            return getHeight(root);
+        }
+    }
+
+
+    private int getHeight(BinaryTreeNode node){
+        if (node==null)
+        {
+            return -1;
+        }
+        else{
+            int heightL=getHeight(node.getLeftChild());
+            int heightR=getHeight(node.getRightChild());
+            if (heightL>heightR)
+                return heightL+1;
+            else if (heightL<heightR){
+                return heightR+1;
+            }
+            else return heightL+1;
+        }
+    }
+
+
+    private void inOrderTraversal(BinaryTreeNode node, ArrayList<BinaryTreeNode> list){
+
+        if (node!=null)
+        {
+
+            inOrderTraversal(node.getLeftChild(),list);
+            list.add(node);
+            inOrderTraversal(node.getRightChild(),list);
+        }
+
+    }
+
+
+    private void preOrderTraversal(BinaryTreeNode node, ArrayList<BinaryTreeNode> list){
+
+        if (node!=null)
+        {
+            list.add(node);
+            preOrderTraversal(node.getLeftChild(),list);
+            preOrderTraversal(node.getRightChild(),list);
+        }
+
+    }
+
+
+    private void postOrderTraversal(BinaryTreeNode node, ArrayList<BinaryTreeNode> list){
+
+        if (node!=null)
+        {
+            postOrderTraversal(node.getLeftChild(),list);
+            postOrderTraversal(node.getRightChild(),list);
+            list.add(node);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
